556312a4ab692234f767497c234762ad3617dc7c
Arwan Salafudin Ashari <arwansalafudina@gmail.com>
2023-06-16 04:31:49 +0700

    set theme

--- app/src/main/java/me/arwan/mov/ui/theme/Color.kt
+++ app/src/main/java/me/arwan/mov/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package me.arwan.mov.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val primaryColor = Color(0XFF3CD6D6)
+val Orange200 = Color(0xFFFFCC80)
+val Orange500 = Color(0xFFFF9800)
+val Orange700 = Color(0xFFF57C00)
+val Orange900 = Color(0xFFE65100)
+val primaryColor = Color(0xFFF57C00)
