9b761d6a247a6dcc789f2344f3ffbf71b634f62b
Arwan Salafudin Ashari <arwansalafudina@gmail.com>
2023-06-16 10:09:02 +0700

    remove unused table

--- app/src/main/java/me/arwan/mov/models/Movie.kt
+++ app/src/main/java/me/arwan/mov/models/Movie.kt
@@ -1,17 +1,13 @@
 package me.arwan.mov.models
 
 import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import me.arwan.mov.BuildConfig
-import me.arwan.mov.core.constants.Constants
 import me.arwan.mov.models.response.movie.MovieDTO
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 @Parcelize
-@Entity(tableName = "table_movies")
 data class Movie(
     val title: String,
     val originalTitle: String,
@@ -20,7 +16,6 @@ data class Movie(
     val imgMovie: String,
     val imgCover: String,
     val voteAverage: Double,
-    @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
 ) : Parcelable {
     companion object {
